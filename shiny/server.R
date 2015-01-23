fdc03cec64de904808079d85c94862ce0bf2e51d
manhtai <vo@manhtai.com>
2015-01-23 22:47:22 +0700

    add shiny

---
+++ shiny/server.R
@@ -0,0 +1,7 @@
+library(shiny)
+lexicon <- read.csv("lexicon.txt", header=F, stringsAsFactors=F)
+colnames(lexicon) <- c("EN","VI")
+
+shinyServer(function(input, output) {
+	output$mytable <- renderDataTable(lexicon, options = list(pageLength = 25))
+	})
---
+++ shiny/ui.R
@@ -0,0 +1,6 @@
+library(shiny)
+
+shinyUI(navbarPage(
+	title = 'Lexicon',
+	tabPanel('Nhấp vào tiêu đề để sắp xếp', dataTableOutput('mytable'))
+))
