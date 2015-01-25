7f09b4f0c50bc340ff1bc7656237b6e53998c574
manhtai <vo@manhtai.com>
2015-01-25 22:39:50 +0700

    fix lexs

--- server.R
+++ server.R
@@ -1,5 +1,6 @@
 library(shiny)
-lexicon <- read.csv("lexicon.txt", header=F, stringsAsFactors=F)
+lexicon <- read.csv("lexicon.txt", 
+										sep=";", header=F, stringsAsFactors=F)
 colnames(lexicon) <- c("EN","VI")
 shinyServer(function(input, output) {
 	output$mytable <- renderDataTable(lexicon, options = list(pageLength = 25))
